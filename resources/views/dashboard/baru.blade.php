5db253241d85fd850a846638fcd068e6750c0197
unknown <madaevos6@gmail.com>
2023-04-30 16:57:03 +0800

    Membuat login, regis, forgot

---
+++ resources/views/dashboard/baru.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('title','Baru')
+@section('content')
+<section class="section">
+    <div class="section-header">
+      <h1>Baru</h1>
+    </div>
+
+    <div class="section-body">
+        Content Here
+    </div>
+  </section>
+@endsection
+
+@section('sidebar')
+@endsection
